b1321c3f13a879899319ab15fedb4e43221dd387
Max Schrementi <6629474+dwolrdcojp@users.noreply.github.com>
2024-11-20 15:12:48 -0500

    fix prettier on save

--- init.lua
+++ init.lua
@@ -811,6 +811,8 @@ require('lazy').setup({
         -- You can use 'stop_after_first' to run the first available formatter from the list
         javascript = { 'prettierd' },
         typescript = { 'prettierd' },
+        javascriptreact = { 'prettierd' },
+        typescriptreact = { 'prettierd' },
       },
     },
   },
